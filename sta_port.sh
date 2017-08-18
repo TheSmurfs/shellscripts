c6fffdea3c6a8de9cc2c2c65665c6f95a027313c
TheSmurfs <752105005@qq.com>
2017-08-18 09:48:51 +0800

    -a

--- sta_port.sh
+++ sta_port.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Author:Poirot	Email:752105005@qq.com		Date:2017-08-15
-#Version 1.0
+#Version 1.1
 #Function:
 
 sta_port () 
@@ -8,6 +8,11 @@ sta_port ()
 service=`lsof -i:$1|sed -n '2p' |cut -d" " -f1`
 protocol=(`lsof -i:$1|tail -n +2|awk '{print $8}'|sort |uniq`)
 # echo "${protocol[@]}"
+if ((${#protocol[@]}==0))
+then
+    echo "没有程序监听$1端口"
+    exit
+fi    
 for i in ${protocol[@]}
 do
 echo "$service正在侦听:$i$1端口"
